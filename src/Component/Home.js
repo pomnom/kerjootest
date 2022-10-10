f77fac76a319a9b136658c22cdcdf2de1f67bec1
pomnom <yuslibra6@gmail.com>
2022-10-10 13:20:59 +0800

    React Router

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import TestFlex from "./TestFlex";
 import TestGrid from "./TestGrid";
 import TestSelect from "./TextSelect";
@@ -6,16 +6,14 @@ import TestSelect from "./TextSelect";
 const Home = () => {
   return (
     <div>
-      <h1>Ini adalah home</h1>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="test-grid" element={<TestGrid />} />
         <Route path="test-flex" element={<TestFlex />} />
         <Route path="test-select" element={<TestSelect />} />
       </Routes>
-      <li>Test Grid</li>
-      <li>Test Flex</li>
-      <li>Test Select</li>
+      <Link to={"test-grid"}>Test Grid</Link>
+      <Link to={"test-flex"}>Test Flex</Link>
+      <Link to={"test-select"}>Test Select</Link>
     </div>
   );
 };
